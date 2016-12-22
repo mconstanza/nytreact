be1daa883cac46dbd7798f05b3dc5e5da557d693
Mike Constanza <michael.constanza@gmail.com>
2016-12-21 20:51:25 -0500

    config for heroku

---
+++ app/config/config.js
@@ -0,0 +1,9 @@
+var config = {
+  domain: 'mconstanza.auth0.com',
+  auth0Secret: 'SsoEWeTp4FZUNLSOkeviipqq39eAS9mtmHaIz3vhDj098-AA0GTgpcm_CD3ofm4-' ,
+  auth0ClientId: 'hFHhbzhvUj6iii7amGNQs1Uxf7EhxFxI',
+  NYTAPI: "5063f54818154873afa3996286e1b391"
+
+};
+
+module.exports = config;
